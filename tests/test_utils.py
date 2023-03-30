978fbe0c3b6f4fd68cb5a555c372cc74788f2d0a
Vasilis Themelis <vdthemelis@gmail.com>
2023-03-30 13:35:19 +0100

    Serialise `np.ndarray`s. (#30)
    These are fairly common and they end up in DataFrames when reading
    arrays from parquet files through `pd.read_parquet` (among other
    things).

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import numpy as np
 import pytest
 from unittest.mock import Mock
 
@@ -7,7 +8,12 @@ from ipyaggrid.util import Util
 @pytest.mark.parametrize("data,expected", [
     (42, '42'),
     ("Quick brown", '"Quick brown"'),
-    (dt.time(12,0,0), '"12:00:00"')
+    (dt.time(12,0,0), '"12:00:00"'),
+    ([1, 2, 3], '[1, 2, 3]'),
+    (np.full(3, 3), '[3, 3, 3]'),
+    (np.full((3, 3), 3), '[[3, 3, 3], [3, 3, 3], [3, 3, 3]]'),
+    (np.full(1, dt.datetime(2023, 3, 28, 12, 0, 0)), '["2023-03-28T12:00:00"]'),
+    (np.int32(1), '1')
 ])
 def test_json_serialisation_without_data_compression(data, expected):
     widget_mock = Mock(compress_data=False)
